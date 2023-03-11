4f1b9225f19d3df9694c1960edaceb6d99aee324
eddyv <10497078+eddyv@users.noreply.github.com~>
2023-03-11 15:41:36 -0500

    init bevy project

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+pub const HEIGHT: f32 = 1080.0;
+pub const WIDTH: f32 = 1920.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Tower Defense".to_string(),
+                resolution: WindowResolution::new(WIDTH, HEIGHT),
+                ..Default::default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_basic_scene)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 2.0, subdivisions: 0 })),
+        material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+        ..Default::default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.25, 0.0),
+        ..Default::default()
+    });
+}
